304dffbf6c44c44c47e866be59ab986a646229f9
graymount <wenfeng@live.nl>
2025-05-28 21:16:57 +0800

    Initial Commit

---
+++ laohuangli/laohuangliApp.swift
@@ -0,0 +1,17 @@
+//
+//  laohuangliApp.swift
+//  laohuangli
+//
+//  Created by liuwnfng on 2025/5/28.
+//
+
+import SwiftUI
+
+@main
+struct laohuangliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ laohuangliTests/laohuangliTests.swift
@@ -0,0 +1,17 @@
+//
+//  laohuangliTests.swift
+//  laohuangliTests
+//
+//  Created by liuwnfng on 2025/5/28.
+//
+
+import Testing
+@testable import laohuangli
+
+struct laohuangliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
